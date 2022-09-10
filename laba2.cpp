85b1c0679543c0fbddc5b6636b76f836c5c4ac21
tearfu1 <56321070+tearfu1@users.noreply.github.com>
2022-09-10 11:06:44 +0200

    laba2
    5 вариант

---
+++ laba2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+int main()
+{
+	int a,b,c;
+	std::cout << "Input natural numbers A, B, C (separated by a space): ";
+	std::cin >> a >> b >> c;
+	if (a < b && b < c) {
+		std::cout << "Due to A < b < C the result is: " << (c-b-a);
+	}
+	else if ((a % c) == 0) {
+		std::cout << "Due to A is a multiple of C the result is: " << (a/c) + b;
+	}
+	else {
+		std::cout << "Due to your inputs don't match any conditions the result is: " << a+b+c;
+	
+	}
+	
+}
