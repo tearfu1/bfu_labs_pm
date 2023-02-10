311c627cc0cba40540981374b2d80141131b234d
Maxim Vayda <56321070+tearfu1@users.noreply.github.com>
2023-02-11 00:16:52 +0300

    hotfix

--- tasks_sem_2/task2/main.cpp
+++ tasks_sem_2/task2/main.cpp
@@ -4,26 +4,27 @@
 //template - шаблон, для беспроблемного использования итераторов
 template <typename InputIt,typename OutputIt>
 void custom_copy(InputIt begin, InputIt end, OutputIt d_begin){
+    int plus = 8/sizeof(*begin);
         while(begin<end-1){
             auto src = (long long*)begin;
             auto point = (long long*)d_begin;
             *point = *src;
-            begin+=2;
-            d_begin+=2;
+            begin+=plus;
+            d_begin+=plus;
         }
     }
 
 
 int main(){
     const int n = 10;
-    int mas [n];
-    int mas2 [n];
+    char mas [n];
+    char mas2 [n];
     for (int i = 0; i < n; ++i){
         mas[i] = i*2;
     }
     custom_copy(&mas[0],&(mas[0])+n,&mas2[0]);
-    for (auto&& i : mas2){
-        std::cout<<i << " " << &i << std::endl;
+    for (int i = 0;i<n;++i){
+        std::cout<< (int)mas2[i] << std::endl;
     }
     return 0;
 }
