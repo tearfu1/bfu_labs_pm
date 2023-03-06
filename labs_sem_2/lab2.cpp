cc9306effca79bb8696314cc1424fcfb406a9c85
Maxim Vayda <56321070+tearfu1@users.noreply.github.com>
2023-03-07 00:34:32 +0200

    lab2

---
+++ labs_sem_2/lab2.cpp
@@ -0,0 +1,146 @@
+/* осталось сдлеать пункты 6 7 8 9 11 12, сделай за раз пж */
+#include <iostream>
+
+class String
+{
+private:
+	size_t m_size;
+	char* m_str = nullptr;
+private:
+	size_t strlen(const char* start)
+	{
+		const char* end = start;
+		for(;*end != '\0';++end);
+		return end -start;
+	}
+public:
+	String()=default;
+
+	String(int c, size_t size) = delete;
+
+	String(char c, size_t size) : m_size(size), m_str(new char[size+1])
+	{
+		std::fill(m_str,m_str+m_size,c);
+		m_str[m_size] = 0;
+	}
+	String(char* str) : m_size(strlen(str)), m_str(new char[m_size])
+	{
+		std::copy(str,str + m_size,m_str);
+		m_str[m_size] = 0;
+	}
+	String(const String& other) : String(other.m_str) 
+	{
+	}
+	int strcmp(const String& A, const String& B)
+	{
+		int sum = 0;
+		size_t minim = (A.m_size <= B.m_size)*A.m_size + (A.m_size > B.m_size)*B.m_size;
+		for(int i = 0; i < minim; ++i)
+		{
+			std::cout<<(int)A.m_str[i] << " " << (int)B.m_str[i]<<std::endl;
+			sum = (int)A.m_str[i] - (int)B.m_str[i];
+
+			std::cout << sum;
+		}	
+			std::cout << sum;
+		return sum;
+	}
+	String& operator=(String temp)
+	{
+		std::swap(m_size, temp.m_size);
+		std::swap(m_str,temp.m_str);
+		return *this;
+	}
+	String& operator+=(const String& str)
+	{
+		char* temp_m_str = new char[m_size + str.m_size];
+		std::copy(m_str, m_str + m_size, temp_m_str);
+		for(int i = 0; i < str.m_size;++i){
+			temp_m_str[m_size+i] = str.m_str[i];
+		}
+
+		m_size += str.m_size;
+		std::swap(m_str,temp_m_str);
+		
+		return *this;
+	}
+	String operator+(const String& other)
+	{
+		String temp = *this;
+		temp+=other;
+		return temp;
+	}
+	bool operator < (const String& other)
+	{
+        return strcmp(*this, other) < 0;
+    }
+
+    bool operator > (const String& other) 
+	{
+        return strcmp(*this, other) > 0;
+    }
+
+    bool operator == (const String& other) 
+	{
+		return strcmp(*this, other) == 0;
+    }
+
+	size_t length()
+	{
+		return m_size;
+	}
+	char* c_str()
+	{
+		return m_str;
+	}
+	char& operator[](int index)
+	{
+		return m_str[index];
+	}
+	char at(int index)
+	{
+		if(0 <= index && index < m_size)
+		{
+			return m_str[index];
+		}
+		return 0;
+	}
+	int find(char c)
+	{
+		for(int i = 0; i < m_size; ++i)
+		{
+			if (m_str[i] == c) return i;
+		}
+		return -1;
+	}
+	~String()
+	{
+		if(m_str != nullptr) delete[] m_str;
+	}
+	friend std::ostream& operator<<(std::ostream& out,const String& str);
+	friend std::istream& operator>>(std::istream& in, String& str);
+};
+	std::ostream& operator<<(std::ostream& out,const String& str)
+	{
+		for(int i = 0;i < str.m_size + 1; ++i)
+		{
+			out<<str.m_str[i];
+		}
+		return out;
+		
+	}
+	std::istream& operator>>(std::istream& in, String& str)
+	{
+		str.m_size = 1;
+		in>>str.m_str;
+		while(str.m_str[str.m_size]){
+			str.m_size++;
+		}
+		return in;
+	}
+
+int main()
+{	
+	return 0;
+}
+
