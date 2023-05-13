12a838b3c406f5560a1168db47b9143efe0cafe3
tearfu1 <axuior12@gmail.com>
2023-05-13 22:47:24 +0200

    laba7 and task 4 added

---
+++ labs_sem_2/laba7/laba7.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+
+class Shape 
+{
+protected:
+	std::string m_color;
+	int coord_x = 0;
+	int coord_y = 0;
+public:
+	virtual double Area() = 0;
+	virtual void setPosition(int& x, int& y) {
+		this->coord_x = x;
+		this->coord_y = x;
+	}
+	virtual ~Shape() {}
+};
+
+class Triangle : public Shape 
+{
+private:
+	double side_a;
+	double side_b;
+	double side_c;
+public:
+	Triangle(const std::string& color, const double& a = 0, const double& b = 0, const double& c = 0)
+	{
+		this->m_color = color;
+		this->side_a = a;
+		this->side_b = b;
+		this->side_c = c;
+	}
+	double Area() override
+	{
+		double p = (side_a + side_b + side_c) / 2;
+		return sqrt(p * (p - side_a) * (p - side_b) * (p - side_c));
+	}
+	~Triangle() {}
+};
+
+class Rectangle : public Shape
+{
+private:
+	double side_a;
+	double side_b;
+public:
+	Rectangle (const std::string& color, const double& a = 0, const double& b = 0)
+	{
+		this->m_color = color;
+		this->side_a = a;
+		this->side_b = b;
+	}
+	double Area() override
+	{
+		return side_a * side_b;
+	}
+	~Rectangle() {}
+};
+
+class Circle : public Shape
+{
+private:
+	double radius;
+public:
+	Circle(const std::string& color, const double& r = 0)
+	{
+		this->m_color = color;
+		this->radius = r;
+	}
+	double Area() override
+	{
+		return acos(-1) * pow(radius, 2);
+	}
+	~Circle(){}
+};
+int main() {
+	Rectangle rl("red", 2,2);
+	Triangle tl("red", 2,2,2);
+	Circle cl("red", 2);
+	std::cout << cl.Area() << " "<< tl.Area() << " "<< cl.Area();
+	return 0;
+}
---
+++ tasks_sem_2/task4/task4.cpp
@@ -0,0 +1,94 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+
+class Device 
+{
+protected:
+	std::string m_name;
+public:
+	Device(const std::string& name)
+	{
+		this->m_name = name;
+	}
+	virtual void poll() {
+		std::cout << m_name<<std::endl;
+	}
+	virtual ~Device(){}
+};
+class ElectricityMeter : public Device
+{
+public:
+	explicit ElectricityMeter(const std::string& name) : Device(name){}
+};
+class DSIBlock : public Device
+{
+public:
+	explicit DSIBlock(const std::string& name) : Device(name) {}
+};
+class HeatControlBlock : public Device
+{
+public:
+	explicit HeatControlBlock(const std::string& name) : Device(name) {}
+};
+
+
+struct  {
+	ElectricityMeter* em = nullptr;
+	DSIBlock* dsi = nullptr;
+	HeatControlBlock* hc = nullptr;
+}Configuration;
+namespace DeviceBase 
+{
+	std::string EMs[] = {
+		"Меркурий 230", "Нева МТ314", "Энергомера CE308"
+	};
+	std::string DSIs[] = {
+			"Reallab NL-16HV", "Приборэлектро PRE-16", "Энергосервис ЭНМВ-1-24"
+	};
+	std::string HCs[] = {
+			"Ouman S203", "Овен ТРМ232"
+	};
+}
+
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	std::fstream f("conf.txt");
+	std::string currDevice;
+	
+	while (getline(f, currDevice))
+	{
+		int n = sizeof(DeviceBase::EMs) / sizeof(std::string);
+		for (int i = 0; i < n; ++i)
+		{
+			if (DeviceBase::EMs[i] == currDevice)
+			{
+				ElectricityMeter *EM = new ElectricityMeter(currDevice);
+				Configuration.em = EM;
+			}
+		}
+		n = sizeof(DeviceBase::DSIs) / sizeof(std::string);
+		for (int i = 0; i < n; ++i)
+		{
+			if (DeviceBase::DSIs[i] == currDevice)
+			{
+				DSIBlock* DSI = new DSIBlock(currDevice);
+				Configuration.dsi = DSI;
+			}
+		}
+		n = sizeof(DeviceBase::HCs) / sizeof(std::string);
+		for (int i = 0; i < n; ++i)
+		{
+			if (DeviceBase::HCs[i] == currDevice)
+			{
+				HeatControlBlock* HC = new HeatControlBlock(currDevice);
+				Configuration.hc = HC;
+			}
+		}
+	}
+	Configuration.em->poll();
+	Configuration.dsi->poll();
+	Configuration.hc->poll();
+	return 0;
+}
